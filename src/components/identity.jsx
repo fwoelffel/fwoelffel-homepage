1e4829f8c843ec34f1f87e0da1123fda3b814dda
Frédéric Woelffel <woelffel.f@gmail.com>
2019-08-06 09:46:22 +0200

    feat: add avatar

--- src/components/identity.jsx
+++ src/components/identity.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import SocialLinks from './social-links';
+import Img from 'gatsby-image';
+import styles from './identity.module.scss';
 
-const siteMetadataQuery = graphql`
-  query GetSiteMetadata {
+const query = graphql`
+  query {
     site {
       siteMetadata {
         position
         title
       }
     }
+    file(relativePath: { eq: "avatar.jpeg" }) {
+      childImageSharp {
+        fixed(width: 200, height: 200) {
+          ...GatsbyImageSharpFixed_tracedSVG
+        }
+      }
+    }
   }
 `;
 
 const Identity = () => {
-  const { site } = useStaticQuery(siteMetadataQuery);
+  const { site, file } = useStaticQuery(query);
   const { position, title } = site.siteMetadata;
   return (
     <div className='has-text-centered'>
+      <Img
+        className={styles.avatar}
+        fixed={file.childImageSharp.fixed}
+        alt='avatar'
+      />
       <h1 className='title is-1 is-spaced is-uppercase'>{title}</h1>
       <h2 className='subtitle'>{position}</h2>
       <SocialLinks />
