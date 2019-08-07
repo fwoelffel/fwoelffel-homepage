7100ae7ba6cb57014948681634763d556053eadb
Frédéric Woelffel <woelffel.f@gmail.com>
2019-08-07 21:09:27 +0200

    feat: open timeline item links in new tab

--- src/components/timeline-item.jsx
+++ src/components/timeline-item.jsx
@@ -18,7 +18,13 @@ const TimelineItem = ({
       <span className='title is-6'>{what}</span>
       <br />
       <span className='title is-7'>
-        {!!link ? <a href={link}>{where}</a> : where}
+        {!!link ? (
+          <a href={link} target='_blank' rel='noopener noreferrer'>
+            {where}
+          </a>
+        ) : (
+          where
+        )}
       </span>
       <span style={{ 'font-size': '0.8em' }}>
         {!!location ? ' - ' + location : null}
