568381bef8be71f8e394cc092ab3bcf2164d9d91
Frédéric Woelffel <woelffel.f@gmail.com>
2019-08-08 23:37:29 +0200

    feat: extract education to yaml file

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,34 +1,46 @@
 import React from 'react';
 import TimelineItem from './timeline-item';
 import { FaGraduationCap } from 'react-icons/fa';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const Education = () => (
-  <section className='section'>
-    <div className='container'>
-      <h2 className='title is-2'>
-        <FaGraduationCap /> Education
-      </h2>
-      <div className='is-divider' />
-      <div className='timeline'>
-        <TimelineItem
-          start='September 2013'
-          end='January 2016'
-          what='Engineering degree in computer science'
-          where='Conservatoire National des Arts et Métiers'
-          location='Eckbolsheim, France'
-          link='http://iutsd.univ-lorraine.fr/departement-informatique/dut-info/'
-        />
-        <TimelineItem
-          start='September 2011'
-          end='June 2013'
-          what='Two year university degree in computer science'
-          where='IUT Saint-Dié'
-          location='Saint-Dié-des-Vosges, France'
-          link='https://www.cnam-grandest.fr/formation/ING3400A'
-        />
+const query = graphql`
+  query {
+    allEducationYaml {
+      nodes {
+        start
+        location
+        link
+        end
+        what
+        where
+      }
+    }
+  }
+`;
+
+const Education = () => {
+  const { allEducationYaml } = useStaticQuery(query);
+  return (
+    <section className='section'>
+      <div className='container'>
+        <h2 className='title is-2'>
+          <FaGraduationCap /> Education
+        </h2>
+        <div className='is-divider' />
+        <div className='timeline'>
+          {allEducationYaml.nodes.map((education) => (
+            <TimelineItem
+              start={education.start}
+              end={education.end}
+              what={education.what}
+              where={education.where}
+              location={education.location}
+              link={education.link}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  </section>
-);
-
+    </section>
+  );
+};
 export default Education;
