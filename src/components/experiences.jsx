3a8615636960437ebc58edf3a51dfbc719ca2a67
Frédéric Woelffel <woelffel.f@gmail.com>
2019-08-08 18:17:16 +0200

    refactor: remove experiences wrapping container div

--- src/components/experiences.jsx
+++ src/components/experiences.jsx
@@ -4,37 +4,35 @@ import { FaPaperPlane } from 'react-icons/fa';
 
 const Experiences = () => (
   <section className='section'>
-    <div className='container'>
-      <h2 className='title is-2'>
-        <FaPaperPlane /> Experiences
-      </h2>
-      <div className='is-divider' />
-      <div className='timeline'>
-        <TimelineItem
-          start='September 2016'
-          end='Current'
-          what='Backend engineer'
-          where='IHU Strasbourg'
-          location='Strasbourg, France'
-          link='https://www.ihu-strasbourg.eu/'
-        />
-        <TimelineItem
-          start='September 2013'
-          end='August 2016'
-          what='Apprenticeship'
-          where='IRCAD France'
-          location='Strasbourg, France'
-          link='https://www.ircad.fr/'
-        />
-        <TimelineItem
-          start='April 2013'
-          end='June 2013'
-          what='Internship'
-          where='CIC-IT Nancy'
-          location='Nancy, France'
-          link='http://www.cic-it-nancy.fr/en/'
-        />
-      </div>
+    <h2 className='title is-2'>
+      <FaPaperPlane /> Experiences
+    </h2>
+    <div className='is-divider' />
+    <div className='timeline'>
+      <TimelineItem
+        start='September 2016'
+        end='Current'
+        what='Backend engineer'
+        where='IHU Strasbourg'
+        location='Strasbourg, France'
+        link='https://www.ihu-strasbourg.eu/'
+      />
+      <TimelineItem
+        start='September 2013'
+        end='August 2016'
+        what='Apprenticeship'
+        where='IRCAD France'
+        location='Strasbourg, France'
+        link='https://www.ircad.fr/'
+      />
+      <TimelineItem
+        start='April 2013'
+        end='June 2013'
+        what='Internship'
+        where='CIC-IT Nancy'
+        location='Nancy, France'
+        link='http://www.cic-it-nancy.fr/en/'
+      />
     </div>
   </section>
 );
