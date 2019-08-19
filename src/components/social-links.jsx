f92abc7b534187cb44af5d188b1e661479bbb688
Frédéric Woelffel <woelffel.f@gmail.com>
2019-08-19 11:00:17 +0200

    feat: add aria labels to external links

--- src/components/social-links.jsx
+++ src/components/social-links.jsx
@@ -7,21 +7,25 @@ const SocialLinks = ({ size = '2.5em' }) => (
   <div className='is-block'>
     <ExternalLink
       className={styles.socialLink}
+      label='LinkedIn'
       to='https://www.linkedin.com/in/fredericwoelffel/'>
       <FaLinkedin size={size} />
     </ExternalLink>
     <ExternalLink
       className={styles.socialLink}
+      label='Twitter'
       to='https://twitter.com/fwoelffel'>
       <FaTwitter size={size} />
     </ExternalLink>
     <ExternalLink
       className={styles.socialLink}
+      label='Github'
       to='https://github.com/fwoelffel'>
       <FaGithub size={size} />
     </ExternalLink>
     <ExternalLink
       className={styles.socialLink}
+      label='contact'
       to='https://frdricwoelffel.typeform.com/to/TgRdLj'>
       <FaEnvelope size={size} />
     </ExternalLink>
