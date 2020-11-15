5c73ce0c41c901978ca11da5910ea6413bf3c580
Frédéric Woelffel <hello@fwoelffel.me>
2020-11-15 19:17:25 +0100

    ci: use Github actions to build and deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,23 @@
-const fullName = 'Frédéric Woelffel';
-const shortName = 'F. Woelffel';
-const position = 'Back-end engineer';
 require('dotenv').config({
   path: `.env`,
 });
+
 const {
-  NODE_ENV,
-  URL: NETLIFY_SITE_URL = 'https://fwoelffel.me',
-  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
-  CONTEXT: NETLIFY_ENV = NODE_ENV,
-  GITHUB_TOKEN,
+  GRAPHQL_URL,
+  GRAPHQL_TOKEN,
   GOOGLE_ANALYTICS_ID,
+  PUBLIC_URL = 'https://fwoelffel.me',
 } = process.env;
-const isNetlifyProduction = NETLIFY_ENV === 'production';
-const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
+const fullName = 'Frédéric Woelffel';
+const shortName = 'F. Woelffel';
+const position = 'Back-end engineer';
 
 module.exports = {
   siteMetadata: {
     title: fullName,
     position,
-    siteUrl,
+    siteUrl: PUBLIC_URL,
     about: `I'm a Node.js expert and javascript/typescript developer, fluent with Nestjs.`,
   },
   plugins: [
@@ -59,38 +57,18 @@ module.exports = {
         trackingId: GOOGLE_ANALYTICS_ID,
       },
     },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        resolveEnv: () => NETLIFY_ENV,
-        env: {
-          production: {
-            policy: [{ userAgent: '*' }],
-          },
-          'branch-deploy': {
-            policy: [{ userAgent: '*', disallow: ['/'] }],
-            sitemap: null,
-            host: null,
-          },
-          'deploy-preview': {
-            policy: [{ userAgent: '*', disallow: ['/'] }],
-            sitemap: null,
-            host: null,
-          },
-        },
-      },
-    },
     'gatsby-transformer-yaml',
     {
       resolve: 'gatsby-source-graphql',
       options: {
         typeName: 'GitHub',
         fieldName: 'github',
-        url: 'https://api.github.com/graphql',
+        url: GRAPHQL_URL,
         headers: {
-          Authorization: `Bearer ${GITHUB_TOKEN}`,
+          Authorization: `Bearer ${GRAPHQL_TOKEN}`,
         },
       },
     },
+    
   ],
 };
