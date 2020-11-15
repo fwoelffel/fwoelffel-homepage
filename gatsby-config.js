fd1133c367c8d9bce807377a9203a40559b6177d
Frédéric Woelffel <hello@fwoelffel.me>
2020-11-15 15:36:38 +0100

    ci: use Github actions to build and deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,21 +5,16 @@ require('dotenv').config({
   path: `.env`,
 });
 const {
-  NODE_ENV,
-  URL: NETLIFY_SITE_URL = 'https://fwoelffel.me',
-  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
-  CONTEXT: NETLIFY_ENV = NODE_ENV,
   GITHUB_TOKEN,
   GOOGLE_ANALYTICS_ID,
+  PUBLIC_URL = 'https://fwoelffel.me',
 } = process.env;
-const isNetlifyProduction = NETLIFY_ENV === 'production';
-const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 
 module.exports = {
   siteMetadata: {
     title: fullName,
     position,
-    siteUrl,
+    siteUrl: PUBLIC_URL,
     about: `I'm a Node.js expert and javascript/typescript developer, fluent with Nestjs.`,
   },
   plugins: [
@@ -57,27 +52,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: GOOGLE_ANALYTICS_ID,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        resolveEnv: () => NETLIFY_ENV,
-        env: {
-          production: {
-            policy: [{ userAgent: '*' }],
-          },
-          'branch-deploy': {
-            policy: [{ userAgent: '*', disallow: ['/'] }],
-            sitemap: null,
-            host: null,
-          },
-          'deploy-preview': {
-            policy: [{ userAgent: '*', disallow: ['/'] }],
-            sitemap: null,
-            host: null,
-          },
-        },
       },
     },
     'gatsby-transformer-yaml',
