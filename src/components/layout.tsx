0312fd84f70abda3c4bce5a413b4ffbc695f0219
Frédéric Woelffel <hello@fwoelffel.me>
2020-11-15 22:45:10 +0100

    feat: use site metadata query in layout component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,20 +1,36 @@
 import Footer from './footer';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const Layout = ({ children }) => (
-  <>
-    <Helmet>
-      <html lang='en' />
-      <meta charSet='utf-8' />
-      <meta name='description' content='Backend engineer' />
-      <meta name='author' content='Frédéric Woelffel' />
-      <title>Frédéric Woelffel</title>
-      <link rel='canonical' href='https://fwoelffel.me' />
-    </Helmet>
-    <div className='container'>{children}</div>
-    <Footer />
-  </>
-);
+
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        siteUrl
+      }
+    }
+  }
+`;
+
+const Layout = ({ children }) => {
+  const { site: { siteMetadata: { title, siteUrl } } } = useStaticQuery(query);
+  return (
+    <>
+      <Helmet>
+        <html lang='en' />
+        <meta charSet='utf-8' />
+        <meta name='description' content='Backend engineer' />
+        <meta name='author' content='Frédéric Woelffel' />
+        <title>{title}</title>
+        <link rel='canonical' href={siteUrl} />
+      </Helmet>
+      <div className='container'>{children}</div>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
